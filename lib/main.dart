f0624c5ca3f6d049808f323eb9739c15f69d7f05
Gaabi <gaabi@kali>
2025-10-13 07:17:27 -0500

    mana hammasi kot boldi-yu rasm qoshildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:version1/Screens/admin_panel_screen.dart';
+import 'package:version1/Screens/chat_screen.dart';
+import 'package:version1/Screens/create_post_screen.dart';
+import 'package:version1/Screens/home_screen.dart';
+import 'package:version1/Screens/language_selection_screen.dart';
+import 'package:version1/Screens/login_screen.dart';
+import 'package:version1/Screens/post_detail_screen.dart';
+import 'package:version1/Screens/profile_screen.dart';
+import 'package:version1/Screens/register_screen.dart';
+import 'package:version1/Screens/search_screen.dart';
+import 'package:version1/Screens/splash_screen.dart';
+import 'package:version1/Screens/stats_screen.dart';
+import 'package:version1/translation.dart';
 
-void main() {
-  runApp(const MyApp());
+
+void main() async {
+  await GetStorage.init(); // Offline storage
+  runApp(JobHunterApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class JobHunterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      title: 'Job Hunter',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        primaryColor: Colors.blueAccent,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.blue,
+          accentColor: Colors.teal,
+        ),
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: GoogleFonts.robotoTextTheme(),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blueAccent,
+          elevation: 0,
+          centerTitle: true,
+        ),
+        cardTheme: CardThemeData(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          elevation: 4,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            padding: EdgeInsets.symmetric(vertical: 16),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      translations: AppTranslations(),
+      locale: GetStorage().read('language') != null
+          ? Locale(GetStorage().read('language').split('_')[0], GetStorage().read('language').split('_')[1])
+          : Locale('uz', 'UZ'),
+      fallbackLocale: Locale('uz', 'UZ'),
+      home: SplashScreen(),
+      getPages: AppRoutes.routes,
     );
   }
+}
+
+class AppRoutes {
+  static final routes = [
+    GetPage(name: '/splash', page: () => SplashScreen()),
+    GetPage(name: '/language', page: () => LanguageSelectionScreen()),
+    GetPage(name: '/login', page: () => LoginScreen()),
+    GetPage(name: '/register', page: () => RegisterScreen()),
+    GetPage(name: '/home', page: () => HomeScreen()),
+    GetPage(name: '/search', page: () => SearchScreen()),
+    GetPage(name: '/chat', page: () => ChatScreen()),
+    GetPage(name: '/profile', page: () => ProfileScreen()),
+    GetPage(name: '/create_post', page: () => CreatePostScreen()),
+    GetPage(name: '/post_detail', page: () => PostDetailScreen()),
+    GetPage(name: '/stats', page: () => StatsScreen()),
+    GetPage(name: '/admin', page: () => AdminPanelScreen()),
+  ];
 }
